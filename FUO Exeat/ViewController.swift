5bed08c168cfb4c679f0d45bfa53091201857ebc
SODIQ OLADENI <sodiqoladeni@gmail.com>
2019-10-15 12:41:36 +0100

    Initial Commit

---
+++ FUO Exeat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FUO Exeat
+//
+//  Created by mac on 15/10/2019.
+//  Copyright © 2019 NotZero Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
