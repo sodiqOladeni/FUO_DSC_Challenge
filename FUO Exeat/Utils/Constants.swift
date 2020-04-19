df811dc60842a67f5fd903c3bdf28e1a75afc902
SODIQ OLADENI <sodiqoladeni@gmail.com>
2020-04-19 17:46:21 +0100

    setting up ftue commit

---
+++ FUO Exeat/Utils/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  FUO Exeat
+//
+//  Created by sodiqOladeni on 18/04/2020.
+//  Copyright © 2020 NotZero Technologies. All rights reserved.
+//
+
+import Foundation
---
+++ FUO Exeat/Utils/UtilFuction.swift
@@ -0,0 +1,9 @@
+//
+//  UtilFuction.swift
+//  FUO Exeat
+//
+//  Created by sodiqOladeni on 19/04/2020.
+//  Copyright © 2020 NotZero Technologies. All rights reserved.
+//
+
+import Foundation
