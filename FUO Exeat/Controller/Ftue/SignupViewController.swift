c2dee0eb9adf5e9f2bd9c9cc491d216ae26e7d0b
SODIQ OLADENI <sodiqoladeni@gmail.com>
2020-04-19 19:49:51 +0100

    Some other left over files

---
+++ FUO Exeat/Controller/Ftue/SignupViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignupViewController.swift
+//  FUO Exeat
+//
+//  Created by Sodiq on 16/10/2019.
+//  Copyright © 2019 NotZero Technologies. All rights reserved.
+//
+import UIKit
+
+class SignupViewController: UIViewController {
+    
+    var userType:Int!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
